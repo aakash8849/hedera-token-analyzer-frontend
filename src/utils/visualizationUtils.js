456901b1d435cc04456bdaa369a909fa2fba0162
aakash8849 <aakash000bpatel@gmail.com>
2024-12-17 02:27:55 +0530

    Update visualizationUtils.js

--- src/utils/visualizationUtils.js
+++ src/utils/visualizationUtils.js
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
 
-export function processVisualizationData(data, tokenInfo) {
+export function processVisualizationData(data) {
   // Parse CSV data
   const holders = data.holders.split('\n')
     .slice(1)
@@ -13,7 +13,12 @@ export function processVisualizationData(data, tokenInfo) {
       };
     });
 
-  const treasuryId = tokenInfo.treasury_account;
+  // Find treasury account (account with highest balance)
+  const treasuryAccount = holders.reduce((max, holder) => 
+    holder.balance > max.balance ? holder : max
+  , holders[0]);
+
+  const treasuryId = treasuryAccount.account;
   const totalSupply = holders.reduce((sum, h) => sum + h.balance, 0);
 
   // Calculate balance ranges for visualization
